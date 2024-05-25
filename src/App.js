31e09a71761eaedf59d3bf8f20fc63db1b727b49
willam2007 <vokasur5707@gmail.com>
2024-05-25 11:37:39 +1000

    [footer-1] Добавлен footer.js, реаилзован компонент footer

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./components/home/HomePage";
 import Header from "./components/widGet/Header";
+import Footer from "./components/widGet/Footer";
+
 
 function App() {
+	const conorImgUrl= "https://img.gazeta.ru/files3/360/17276360/upload-RIA_5592197-pic_32ratio_900x600-900x600-8633.jpg"
 	return (
 		<div className="App">
 			<Header />
 			<HomePage />
+			<Footer imageUrl = {conorImgUrl}/> 
 		</div>
+		
 	);
 }
 
